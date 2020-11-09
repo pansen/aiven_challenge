22870b092a515c6d5fc8aff1e0b01b69e434d2aa
andi <andi@zerotired.com>
2020-11-09 16:29:19 +0100

    Add some basic interaction tests

--- pansen/aiven/conftest.py
+++ pansen/aiven/conftest.py
@@ -1,14 +1,19 @@
+import asyncio
 import inspect
 import os
 from asyncio.selector_events import BaseSelectorEventLoop
 
 import pytest
-from aiokafka import AIOKafkaProducer
+from aiokafka import AIOKafkaConsumer, AIOKafkaProducer
 from starlette.testclient import TestClient
 from vcr import VCR
 
 from pansen.aiven.config import Config, configure
 from pansen.aiven.main import app
+
+KAFKA_TEST_TOPIC = "kafka_topic_testing"
+KAFKA_TEST_GROUP = "kafka_group_testing"
+KAFKA_PARTITION = 0
 
 
 @pytest.fixture(scope="function")
@@ -27,11 +32,35 @@ async def asyncio_kafka_producer(config: Config, event_loop: BaseSelectorEventLo
     producer = AIOKafkaProducer(loop=event_loop, bootstrap_servers=config.KAFKA_SERVER, enable_idempotence=True)
     # Get cluster layout and initial topic/partition leadership information
     await producer.start()
+    await wait_topic(producer.client, KAFKA_TEST_TOPIC)
+
     try:
         yield producer
     finally:
         # Wait for all pending messages to be delivered or expire.
         await producer.stop()
+
+
+@pytest.fixture(scope="function")
+async def asyncio_kafka_consumer(config: Config, event_loop: BaseSelectorEventLoop) -> AIOKafkaConsumer:
+    consumer = AIOKafkaConsumer(
+        KAFKA_TEST_TOPIC,
+        loop=event_loop,
+        bootstrap_servers=config.KAFKA_SERVER,
+        group_id=KAFKA_TEST_GROUP,
+        # https://github.com/aio-libs/aiokafka/blob/f7f55b19b43b084edc844c3531a570d233d37912/tests/test_consumer.py#L34-L37
+        auto_offset_reset="earliest",
+        enable_auto_commit=True,
+        request_timeout_ms=300,
+    )
+    # Get cluster layout and join group
+    await consumer.start()
+    await consumer.seek_to_committed()
+    try:
+        yield consumer
+    finally:
+        # Will leave consumer group; perform autocommit if enabled.
+        await consumer.stop()
 
 
 def _build_vcr_cassette_yaml_path_from_func_using_module(function):
@@ -46,3 +75,20 @@ cast_vcr = VCR(
     # 'new_episodes' | 'once'
     record_mode="once",
 )
+
+
+async def wait_topic(client, topic):
+    """
+    Stolen from
+    https://github.com/aio-libs/aiokafka/blob/f7f55b19b43b084edc844c3531a570d233d37912/tests/_testutil.py#L347-L357
+    """
+    client.add_topic(topic)
+    for _i in range(5):
+        ok = await client.force_metadata_update()
+        if ok:
+            ok = topic in client.cluster.topics()
+        if not ok:
+            await asyncio.sleep(1)
+        else:
+            return
+    raise AssertionError('No topic "{}" exists'.format(topic))
--- pansen/aiven/tests/test_playground.py
+++ pansen/aiven/tests/test_playground.py
@@ -1,7 +1,36 @@
 import pytest
-from aiokafka import AIOKafkaProducer
+from aiokafka import AIOKafkaConsumer, AIOKafkaProducer
+from aiokafka.structs import RecordMetadata
+from kafka import TopicPartition
+
+from pansen.aiven.conftest import KAFKA_PARTITION, KAFKA_TEST_TOPIC
 
 
 @pytest.mark.asyncio
 async def test_kafka_producer(asyncio_kafka_producer: AIOKafkaProducer):
-    await asyncio_kafka_producer.send_and_wait("my_topic", b"Super message")
+    res = await asyncio_kafka_producer.send_and_wait(KAFKA_TEST_TOPIC, b"Super message")
+    assert isinstance(res, RecordMetadata)
+
+
+@pytest.mark.asyncio
+async def test_kafka_consumer_partition(asyncio_kafka_consumer: AIOKafkaConsumer):
+    assert [
+        TopicPartition(KAFKA_TEST_TOPIC, KAFKA_PARTITION),
+    ] == sorted(list(asyncio_kafka_consumer.assignment()))
+
+
+@pytest.mark.asyncio
+async def test_kafka_consumer_consume(
+    asyncio_kafka_producer: AIOKafkaProducer, asyncio_kafka_consumer: AIOKafkaConsumer
+):
+    _future = await asyncio_kafka_producer.send(KAFKA_TEST_TOPIC, b"Super message", partition=KAFKA_PARTITION)
+    resp = await _future
+    assert KAFKA_PARTITION == resp.partition
+    await asyncio_kafka_consumer.seek_to_committed()
+
+    # Consume messages
+    async for msg in asyncio_kafka_consumer:
+        print("consumed: ", msg.topic, msg.partition, msg.offset, msg.key, msg.value, msg.timestamp)
+        return
+
+    assert False, "Did not consume anything"
